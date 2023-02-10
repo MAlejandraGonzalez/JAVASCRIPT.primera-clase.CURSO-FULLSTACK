c5f7708b612bce939efcca677cb7a838c2e47c68
MAlejandraGonzalez <malejandragonzalez95@gmail.com>
2023-02-10 00:17:02 -0300

    primera subida

---
+++ app.js
@@ -0,0 +1,12 @@
+alert('vemos el mensaje de alerta');
+var confirmacion = confirm('desea continuar?');
+var ingresar = prompt('Escribe tu nombre');
+var ingresar = prompt('Escribe tu edad');
+if(edad >= 18){
+
+    alert('usted es mayor de edad');
+
+}else{
+
+    alert('usted es menor de edad');
+}
